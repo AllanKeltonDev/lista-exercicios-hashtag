fb74d2ebf74929f1dee6bff37b3baa51fd950e83
DF_FINANCEIRO06 <DF_FINANCEIRO06@adm.rel>
2025-09-22 18:34:41 -0300

    Atualizações exercicio, upgrade em botão Remover item e alteração do paragrafo ao marcar checkbox

--- dom_formularios_localStorage/to-do-list/script.js
+++ dom_formularios_localStorage/to-do-list/script.js
@@ -1,18 +1,32 @@
 function addTask() {
-  const ul = document.getElementById('taskList');
-  const li = document.createElement('li');
-  const input = document.getElementById('taskInput');
+  const ul = document.getElementById("taskList");
+  const li = document.createElement("li");
+  li.className = "taskText";
+  const input = document.getElementById("taskInput");
+  const paragraph = document.createElement("p");
+  paragraph.className = "taskText";
 
-  const removeButton = document.createElement('button');
-  removeButton.textContent = 'Remover Item';
-  removeButton.id = 'removeButton';
+  const removeButton = document.createElement("button");
+  removeButton.textContent = "Remover Item";
+  removeButton.className = "removeButton";
+  removeButton.onclick = function () {
+    removeTask(this);
+  };
 
-  const checkbox = document.createElement('input');
-  checkbox.type = 'checkbox';
-  checkbox.className = 'checkboxTask';
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.className = "checkboxTask";
 
   ul.appendChild(li);
+  li.appendChild(paragraph);
   li.appendChild(removeButton);
-  li.textContent = input.value;
+  paragraph.textContent = input.value;
   li.prepend(checkbox);
+  input.value = "";
+}
+
+function removeTask(button) {
+  const li = button.parentNode;
+  const ul = li.parentNode;
+  ul.removeChild(li);
 }
